29bf19495fe962cdb0061500fcf0623fdeac5896
nokchax <nokchax@gmail.com>
2019-08-15 18:07:40 +0900

    주테이블에 외래키 : 일대일 양방향 연관관계

--- ex1/src/main/java/Locker.java
+++ ex1/src/main/java/Locker.java
@@ -1,12 +1,15 @@
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Locker {
     @Id @GeneratedValue
     private Long id;
     private String name;
+    @OneToOne(mappedBy = "locker")
+    private Member member;
 
     public Long getId() {
         return id;
